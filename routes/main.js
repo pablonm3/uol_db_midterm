09b9dd143d7ac65cdb10e2dfea917b6535d81a42
pablo marino <pablo-n-m@hotmail.com>
2022-07-03 12:25:52 -0300

    status page done

--- routes/main.js
+++ routes/main.js
@@ -50,15 +50,36 @@ module.exports = function(app) {
     });
 
     
-    app.get("/devices", function(req, res) {
+    app.get("/status", function(req, res) {
         // query database to get all the books
-        let sqlquery = "SELECT * FROM devices";
+        let sqlquery = "SELECT name FROM devices";
         // execute sql query
         db.query(sqlquery, (err, result) => {
             if (err) {
-                res.redirect("/");
+                console.error("err: ", err)
+                res.render("status.ejs",  {status:"error"});
             }
-            res.send(result)
+            else{
+                console.log("devices result: ", result)
+                res.render("status.ejs",  {status:"ok", devices: result});
+            }
+        });
+    });
+
+    app.get("/status_sp", function(req, res) {
+        // query database to get all the books
+        console.log("req.query: ", req.query)
+        let sqlquery = "SELECT * FROM devices WHERE name = ?";
+        // execute sql query
+        db.query(sqlquery, req.query.name, (err, result) => {
+            if (err) {
+                console.error("err: ", err)
+                res.render("status_sp.ejs",  {status:"error"});
+            }
+            else{
+                console.log("devices result: ", result)
+                res.render("status_sp",  {status:"ok", device: result[0]});
+            }
         });
     });
 
